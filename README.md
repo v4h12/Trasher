d26dca0f5b732b1d71fae9cd0b2df9457276b6cc
Ethan Scott <ethanscottza@outlook.com>
2025-12-31 16:54:40 +0200

    finally added in the repo!

---
+++ README.md
@@ -0,0 +1,33 @@
+# Trasher
+
+## Meet Trasher
+
+Trasher is a basic CLI program made with python that manages your trash and makes in simple.
+
+**How to Install:**
+
+- Start off by git cloning the repository
+- CD into 'Trasher'
+- Startup the install script with './install.sh'
+- Then Bob's your uncle, you're ready to go!
+
+**The way it works:**
+
+- Type 'trasher' into your terminal, the program will start and immediately list any files or directories in your trash.
+- You can decide whether you want to restore or delete anything with 'r/d'.
+- Either select all or individual files which are numbered within the list.
+- Type in which number you'd like to restore or delete and hit enter to let Trasher do its thing!
+
+**Note**
+~ any files that are protected through root privileges or permissions can be deleted with 'sudo trasher' or you will be prompted with your password when you are at that point.
+
+**To Add:**
+
+- I plan on adding a TUI version with the curses library... More to come.
+- There is also a plan on adding in flags for more specific usage of Trasher
+
+**Inspiration**
+This is my very first actual project that I have created from scratch, I have spent the past year learning python - having no previous coding experience. It has been quite the journey so far and these projects are what I enjoy making - something useful, simple and fun!
+In lieu of the many free resources I myself have used, i thought it would only be fitting to give something in return, regardless of how small it may be.
+
+Please feel free to make any suggestions, pull requests or anything - it will all be appreciated.
---
+++ main.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+import os
+from urllib.parse import unquote
+import shutil
+import subprocess
+
+# using sudo -E will exclude a given path if in env (preserves variables)
+
+# grab directory to create variables of the output (files, info, expunged)
+tr_path = os.path.join(os.environ["HOME"], ".local/share/Trash")
+tr_files = os.path.join(tr_path, "files")
+tr_info = os.path.join(tr_path, "info")
+
+
+# this is to get the original path and decode it for the restore_file function
+def decog_file(info_file):
+    if not os.path.exists(info_file):
+        return None
+
+    with open(info_file) as f:
+        for line in f:
+            if line.startswith("Path="):
+                return unquote(line.strip().split("=", 1)[1])
+    return None
+
+
+# replace variable content with og_path using os.rename
+def restore_file(filename):
+    trash_file = os.path.join(tr_files, filename)
+    info_file = os.path.join(tr_info, filename + ".trashinfo")
+
+    og_path = decog_file(info_file)
+    if not og_path:
+        print(f"Cannot restore {filename}: missing path or possibly missing .trashinfo")
+        return
+
+    # first is file to be replaced, second is the file used to replace (os.rename)
+    os.makedirs(os.path.dirname(og_path), exist_ok=True)
+    os.rename(trash_file, og_path)
+    os.remove(info_file)
+    print(f"restored {filename} to {og_path}")
+
+    # os.path.basename(og_path)
+
+
+# delete function
+def delete_file(filename):
+    trash_file = os.path.join(tr_files, filename)
+    info_file = os.path.join(tr_info, filename + ".trashinfo")
+
+    try:
+        if os.path.isdir(trash_file):
+            shutil.rmtree(trash_file)
+        else:
+            os.remove(trash_file)
+
+        if os.path.exists(info_file):
+            os.remove(info_file)
+        print(f"Permanently deleted {filename}")
+
+    # create permission option to change ownership of protected file
+    except PermissionError:
+        print(f"\nPermission denied: {filename} is protected")
+        chper = get_choice(
+            "Requires root access to delete, Continue? (y/n): ", ["y", "n"]
+        )
+
+        if chper == "y":
+            try:
+                print("requesting sudo access...")
+                perch = subprocess.run(
+                    [
+                        "sudo",
+                        "chown",
+                        "-R",
+                        f"{os.environ['USER']}:{os.environ['USER']}",
+                        trash_file,
+                    ],
+                    check=True,
+                    stderr=subprocess.PIPE,
+                )
+
+                modch = subprocess.run(
+                    [
+                        "sudo",
+                        "chmod",
+                        "-R",
+                        "u+rw",
+                        trash_file,
+                    ],
+                    check=True,
+                )
+
+                print(f"Ownership changed for {filename}. Deleting... ")
+
+                # run files to delete again
+                if os.path.isdir(trash_file):
+                    shutil.rmtree(trash_file)
+                else:
+                    os.remove(trash_file)
+
+                if os.path.exists(info_file):
+                    os.remove(info_file)
+                print(f"Succesfully deleted {filename}")
+
+            except subprocess.CalledProcessError as e:
+                print(f"failed to gain root permission {e.stderr.decode().strip()}")
+            except Exception as e:
+                print(f"Error after permission change {e}")
+
+        elif chper == "n":
+            print(f"skipping {filename}...")
+
+
+# choice function for invalid inputs
+def get_choice(prompt, valid_options):
+    while True:
+        choice = input(prompt).lower()
+        if choice in valid_options:
+            return choice
+        print("Invalid Input")
+
+
+# function for handling numbered inputs for files
+def get_file_num(prompt, max_files):
+    while True:
+        try:
+            choice = input(prompt)
+            nums = choice.split()
+            indices = []
+            for num in nums:
+                index = int(num) - 1
+                if index < 0 or index >= max_files:
+                    print(f"Number {num} is out of range: (1-{max_files})")
+                    raise ValueError
+                indices.append(index)
+            return indices
+        except ValueError:
+            print("Invalid input")
+
+
+def format_size(bytes):
+    for unit in ["B", "KB", "MB", "GB"]:
+        if bytes < 1024:
+            return f"{bytes:.1f}{unit}"
+        bytes /= 1024
+    return f"{bytes:.1f}TB"
+
+
+# print filenames in the info dir
+if not os.listdir(tr_files):
+    print("Trash is empty")
+    exit()
+else:
+    print("Files in trash: ")
+    files = os.listdir(tr_files)
+    for i, filename in enumerate(files, 1):
+        file_path = os.path.join(tr_files, filename)
+        if os.path.isdir(file_path):
+            size_bytes = os.stat(os.path.join(tr_files, filename)).st_size
+            print(f" {i}. {filename}(/) - {format_size(size_bytes)}")
+        else:
+            size_bytes = os.stat(os.path.join(tr_files, filename)).st_size
+            print(f" {i}. {filename} - {format_size(size_bytes)}")
+
+# user input
+what = get_choice("Restore or Delete files? (r/d): ", ["r", "d"])
+
+# if you chose 'r' to restore files
+if what == "r":
+    resaf = get_choice("Restore all files? (y/n): ", ["y", "n"])
+    if resaf == "y":
+        for filename in os.listdir(tr_files):
+            restore_file(filename)
+
+    elif resaf == "n":
+        indices = get_file_num("Select number/s to restore: ", len(files))
+        for index in indices:
+            restore_file(files[index])
+
+# if you choose 'd' to delete files
+elif what == "d":
+    delaf = get_choice("Delete all files? (y/n): ", ["y", "n"])
+    if delaf == "y":
+        for filename in os.listdir(tr_files):
+            delete_file(filename)
+
+    elif delaf == "n":
+        indices = get_file_num("Select number/s to delete: ", len(files))
+        for index in indices:
+            delete_file(files[index])
+
+# -------------------------------------------------#
+# **FIXED**
+# issue: when deleted files are recovered, they get restored in a directory with the files inside and name instead
+# of recovering to original position as the file which was deleted
+
+# issue: .trashinfo out of the for loop
+
+# - packages installed via a package manager will give a permission error:
+#   giving trasher root permissons doesnt help since there is a diff trash dir in root
+#   possible fixes:
+#       - force it to delete
+#       - make script to change file permission (with permission from user) to delete
+#   (for now give permission error so program doesnt crash)
+# -------------------------------------------------#
+
+# TO ADD:
+# - deletion date
